dd243851750e26d70e0917ecf332c71a25097999
Tarskan <55380090+Tarskan@users.noreply.github.com>
2020-09-28 14:18:06 +0200

    starter pack

---
+++ vue/index.php
@@ -0,0 +1,82 @@
+<html lang="fr">
+    <head>
+        <title>P.M.U</title>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="../css/indexcss.css">
+        <script src="https://kit.fontawesome.com/28132c3659.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark"> 
+            <a id="titre">P.M.U</a>
+            <div class="navbar-collapse collapse order-3 dual-collapse2"> 
+                <ul class="navbar-nav ml-auto"> 
+                    <li class="nav-item"> 
+                    <a class="nav-link" href="#">Compte</a> 
+                    </li> 
+                    <li class="nav-item"> 
+                    <a class="nav-link" href="#"><i class="fas fa-address-book"></i> Panier</a> 
+                    </li> 
+                </ul> 
+            </div> 
+        </nav> 
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produit</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Conseil</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </header>
+        <main>
+           <div>
+               <div class="row ml-1 p-2">
+                    <div class="col-3">
+                        <div class="row shadow-sm p-3 m-1 mb-5 bg-white rounded">
+                            <div class="row p-3 mb-2 bg-dark text-white shadow-sm p-3 mb-5 rounded">
+                                <a>Recherche produit</a>
+                            </div>
+                            <div class="row shadow-sm p-3 mb-5 bg-white rounded">
+
+                                <button type="button" class="btn btn-primary">Ok</button>
+                            </div>
+                        </div>
+                        <div class="row shadow-sm m-1 p-3 mb-5 bg-white rounded">
+                            <a>Catégorie</a>
+                            <ul>
+                                <li>vis</li>
+                            </ul>
+                        </div>
+                        <div class="row shadow-sm m-1 p-3 mb-5 bg-white rounded">
+                            <a>Conseil</a>
+                        </div>
+                    </div>
+                    <div class="col-5 shadow-sm m-1 p-2 mb-5 bg-white rounded">
+                        <h2>Promotion</h2> 
+                    </div>
+                    <div class="col-3 shadow-sm m-1 p-2 mb-5 bg-white rounded">
+                        <a>Meilleurs Vente</a>
+                        <ul>
+                            <li>vis</li>
+                        </ul>
+                    </div>    
+               </div>
+           </div>
+        </main>
+        <footer>
+            <div>                
+            </div>
+        </footer>
+
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.js"></script>
+    </body>
+</html>
