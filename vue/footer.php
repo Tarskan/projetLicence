8f15ce9408f1fe8f098282ab4e454f0a146941ba
Flokening <66185891+Flokening@users.noreply.github.com>
2020-09-30 09:04:29 +0200

    Ajout du footer

---
+++ vue/footer.php
@@ -0,0 +1,16 @@
+<footer>
+	<div class="container-fluid bg-dark fixed-bottom text-center text-white p-3">
+   		<div class="row">
+      		<div class="col">
+         		<a href="">A propos</a> . 
+         		<a href="">Vie privée</a> . 
+         		<a href="">Condition d'utilisation</a>
+      		</div>
+   		</div>
+   		<div class="row">
+   			<div class="col">
+   				<i class="far fa-copyright"></i> 2020 ******, Inc. All rights reserved.
+   			</div>
+   		</div>
+	</div>	
+</footer>
